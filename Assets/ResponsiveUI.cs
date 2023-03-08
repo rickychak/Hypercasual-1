486853901330df579c6b73151d0caa5fc4f8b122
rickychak <47154254+rickychak@users.noreply.github.com>
2023-03-09 02:09:23 +0800

    responsive UI refactor to utisl

--- Assets/ResponsiveUI.cs
+++ Assets/ResponsiveUI.cs
@@ -1,27 +1,22 @@
-using System;
 using UnityEngine;
-
-namespace RickyUIUtils
+using RickyUIUtils;
+public class ResponsiveUI : MonoBehaviour
 {
-    public class ResponsiveUI : MonoBehaviour
+    private UIUtils _uiUtils;
+    [SerializeField] private Vector2 _iPhone12Resolution = new Vector2(1170, 2532);
+    [SerializeField] private Camera _cam;
+    [SerializeField] private GameObject _uiMom;
+    
+    private void Awake()
     {
-        [SerializeField] private Camera _cam;
-        [SerializeField] private GameObject _UIMother;
-        private Vector2 _referenceResolution;
-        private Vector2 _referenceScale;
-        private Vector2 _originalScale;
-        private Transform _UIMothertransform;
-        private void Start()
-        {
-            _referenceResolution = new Vector2(1170, 2532);
-            _UIMothertransform = _UIMother.transform;
-            _originalScale = _UIMothertransform.localScale; //255, 255, 0
-            
-            _referenceScale.y = (_cam.pixelHeight/_referenceResolution.y)*_originalScale.y;
-            _referenceScale.x = (_cam.pixelWidth/_referenceResolution.x)*_originalScale.x;
-            _UIMothertransform.localScale = _referenceScale;
-            
-        }
+        _cam = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
+        _uiMom = GameObject.FindGameObjectWithTag("UI");
+        _uiUtils = new UIUtils(_cam, _uiMom, _iPhone12Resolution);
     }
-}
 
+    void Start()
+    {
+        _uiUtils.SetResolutionOnStart();
+    }
+
+}
